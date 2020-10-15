8fd2f66309a92a0fc17df97f61e71640ef124124
gabriel84k <43577537+gabriel84k@users.noreply.github.com>
2020-10-15 17:54:29 -0300

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,9 @@ Route::middleware('auth:api')->get('/user','Api\UserController@AuthRouteAPI');
 Route::group(['prefix' => '/Sigex'], function () {
     
   Route::resource('CreateUser', 'Api\UserController');
+  
+  
+  Route::delete('EliminarSucursal/{id}/{nivel}','Api\UserController@Destroy_sucursal');
 
   Route::resource('Controles', 'Api\DatosController');
 
@@ -29,7 +32,9 @@ Route::group(['prefix' => '/Sigex'], function () {
  
   Route::resource('Planillas', 'Api\PlanillasController');
   
-
+  Route::resource('SPE', 'Api\SPEController');
+      
+  
   Route::group(['prefix' => '/Conf'], function () {
     Route::get('/clear-cache', function() {
         $exitCode = Artisan::call('config:clear');
@@ -43,7 +48,11 @@ Route::group(['prefix' => '/Sigex'], function () {
       
       return 'Sitio puesto en Mantención'; 
     });
-
+    Route::get('/link', function() {
+      $exitCode = Artisan::call('storage:link');
+      
+      return 'Crea link en public para storage.'; 
+    });
   });
 
 });
